47b39aac82bd718cabb10980209deddf149ff830
chazzzzz01 <honrejaschacha@gmail.com>
2025-06-27 22:40:28 +0900

    5th commit

--- app/main.py
+++ app/main.py
@@ -41,7 +41,9 @@ async def set_alarm(request: AlarmRequest):
     except Exception as e:
         return {"status": "error", "message": str(e)}
 
+
+
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
