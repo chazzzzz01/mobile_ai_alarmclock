15e2e3f9244e3d2a9e58fe3b2641848904f99f81
chazzzzz01 <honrejaschacha@gmail.com>
2025-06-27 19:10:57 +0900

    Fix main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://alarm-clock-umber.vercel.app/",  # ✅ replace with actual deployed frontend domain
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
